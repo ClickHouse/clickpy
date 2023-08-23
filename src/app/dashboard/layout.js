b359a0a5b43f84d90f7fb06dfb41796482503fd7
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-08-23 13:53:12 +0100

    body padding bottom

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -6,39 +6,37 @@ import Link from "next/link";
 
 export default async function DashboardLayout({ children }) {
   return (
-    <>
-      <div>
-        <div className="hidden fixed inset-y-0 z-50 xl:flex w-20 flex-col">
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-nav px-2 pb-2">
-            <div className="flex h-16 shrink-0 items-center mt-6 ml-0">
-              <Link href="/">
-                <Image
-                  className="h-16 w-16"
-                  src="/logo.svg"
-                  alt="ClickPy"
-                  width={41}
-                  height={42}
-                />
-              </Link>
-            </div>
-            <nav className="flex flex-1 flex-col">{/* maybe later nav  */}</nav>
+    <div className="pb-20">
+      <div className="hidden fixed inset-y-0 z-50 xl:flex w-20 flex-col">
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-nav px-2 pb-2">
+          <div className="flex h-16 shrink-0 items-center mt-6 ml-0">
+            <Link href="/">
+              <Image
+                className="h-16 w-16"
+                src="/logo.svg"
+                alt="ClickPy"
+                width={41}
+                height={42}
+              />
+            </Link>
           </div>
+          <nav className="flex flex-1 flex-col">{/* maybe later nav  */}</nav>
         </div>
-        <div className="flex flex-row justify-between items-center p-6 lg:px-8">
-          <div className="xl:ml-28 ml-10">
-            <Search />
-          </div>
-          <div className="hidden xl:flex justify-end">
-            <div className="mr-2">
-              <QueryToggle />
-            </div>
-            <div className="py-3">
-              <VisitClickHouseCloud />
-            </div>
-          </div>
-        </div>
-        {children}
       </div>
-    </>
+      <div className="flex flex-row justify-between items-center p-6 lg:px-8">
+        <div className="xl:ml-28 ml-10">
+          <Search />
+        </div>
+        <div className="hidden xl:flex justify-end">
+          <div className="mr-2">
+            <QueryToggle />
+          </div>
+          <div className="py-3">
+            <VisitClickHouseCloud />
+          </div>
+        </div>
+      </div>
+      {children}
+    </div>
   );
 }
