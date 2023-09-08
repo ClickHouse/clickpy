aa615f8981e8a1c4d6ec3974efc6919f399415ac
Gareth Jones <gareth@clickhouse.com>
2023-09-08 08:06:38 -0400

    Minor mobile fixes for homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,20 @@
 import Header from '@/components/header'
 import Search from '@/components/search'
 import Summary from '@/components/summary'
-import { getTotalDownloads } from "@/utils/clickhouse";
+import { getTotalDownloads } from '@/utils/clickhouse'
 
 import 'server-only'
 
 export default async function Home() {
-  
-  const total_downloads = await getTotalDownloads()
-  
+	const total_downloads = await getTotalDownloads()
+
 	return (
 		<div>
 			{/* Header */}
 			<Header />
 			<main className="isolate h-screen">
 				<div className="pt-14">
-					<div className="pt-24 sm:pt-24">
+					<div className="pt-12 md:pt-24">
 						<div className="max-w-8xl px-6 lg:px-8">
 							<div className="text-center flex items-center flex-col justify-center">
 								<h1 className="text-4xl font-bold font-inter lg:text-5xl">
@@ -23,13 +22,18 @@ export default async function Home() {
 								</h1>
 								<p className="mt-6 text-lg leading-8 text-white">
 									Browse through{' '}
-									<span className="text-primary-300">{Number(total_downloads.projects).toLocaleString("en-US")}</span> Python
-									packages from PyPI and over <span className="text-primary font-bold">{total_downloads.total}</span> 
+									<span className="text-primary-300">
+										{Number(total_downloads.projects).toLocaleString('en-US')}
+									</span>{' '}
+									Python packages from PyPI and over{' '}
+									<span className="text-primary font-bold">
+										{total_downloads.total}
+									</span>
 								</p>
-								<div className="mt-10 flex items-center justify-center gap-x-6">
+								<div className="mt-10 flex items-center justify-center">
 									<Search />
 								</div>
-								<div className="mt-16 flow-root sm:mt-24 w-full md:w-5/6 px-4">
+								<div className="mt-16 flow-root sm:mt-24 w-full md:w-5/6 md:px-4">
 									<Summary />
 								</div>
 							</div>
