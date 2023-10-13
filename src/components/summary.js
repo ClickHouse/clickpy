4a7069a79d01ec289b7442e6e623bc83b7798fd8
Dale Mcdiarmid <dale@clickhouse.com>
2023-10-13 12:05:54 +0100

    new landing

--- src/components/summary.js
+++ src/components/summary.js
@@ -56,13 +56,13 @@ export default function Summary({packages, recent_releases, emerging_repos, need
         </div>
         <div className='xl:col-span-2'>
           <SimpleList link_prefix={'/dashboard/'} data={emerging_repos.map(p => { return { title: p.name, subtitle: `${formatNumber(Number(p.c))} downloads in the last 3 months` } })} 
-          title={<div className='flex space-x-2'><Image alt='recent' src={emergingIcon}/><span className='text-white font-bold space-x-0.5'>Emerging repos</span></div>} subtitle={'Top 5'}/>
+          title={<div className='flex space-x-2'><Image alt='recent' src={emergingIcon}/><span className='text-white font-bold space-x-0.5'>Emerging repos</span></div>} subtitle={`Top ${emerging_repos.length}`}/>
         </div>
         <div className='xl:col-span-2'>
           <SimpleList link_prefix={'/dashboard/'} data={needing_refresh.map(p => { return { title: p.name, subtitle: `${formatNumber(Number(p.c))} downloads, last updated on ${p.last_updated}` } })} 
-          title={<div className='flex space-x-2'><Image alt='recent' src={refreshIcon}/><span className='text-white font-bold space-x-0.5'>Needing a refresh</span></div>} subtitle={'Top 5'}/>
+          title={<div className='flex space-x-2'><Image alt='recent' src={refreshIcon}/><span className='text-white font-bold space-x-0.5'>Needing a refresh</span></div>} subtitle={`Top ${needing_refresh.length}`}/>
         </div>
-        <div className='xl:col-span-2 min-h-[360px]'>
+        <div className='xl:col-span-2'>
           <PunchCard
             data={hot_packages}
             title={<div className='flex space-x-2'><Image alt='recent' src={hotIcon}/><span className='text-white font-bold space-x-0.5'>Hot packages</span></div>}
