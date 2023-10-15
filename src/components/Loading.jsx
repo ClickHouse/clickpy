62f5a06416ad637e24e1a1cedde9433eb32acad7
VINEETH ASOK KUMAR <vineeth.asokkumar@clickhouse.com>
2023-10-15 23:25:47 +0200

    Add loading screen changes

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,16 @@
+import Image from 'next/image';
 import React from 'react';
 
-function Loading(props) {
+function Loading() {
   return (
     <div
-      className='absolute inset-0'
+      className='absolute inset-0 z-30'
     >
       <div className='grid place-items-center h-full w-full'>
-        Loading
+        <div className='flex gap-4'>
+          <Image src="/loading.svg" width={36} height={36} alt='loading-icon' />
+          <span>Loading</span>
+        </div>
       </div>
     </div>)
 }
