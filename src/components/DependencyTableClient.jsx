2e4d12c1284abdefc4467ff34885c699572f434d
Dale Mcdiarmid <dale@clickhouse.com>
2024-10-05 02:57:42 +0100

    make linkable

--- src/components/DependencyTableClient.jsx
+++ src/components/DependencyTableClient.jsx
@@ -1,7 +1,13 @@
 'use client';
-import { ClickUIProvider, Tabs, Table as ClickTable } from '@clickhouse/click-ui'
+import { ClickUIProvider, Tabs, Table as ClickTable, Link } from '@clickhouse/click-ui';
+import {
+    ArrowTopRightOnSquareIcon,
+  } from '@heroicons/react/20/solid';
+import { useState } from 'react';
 
-export default async function DependencyTableClient({ dependencies,  dependents}) {
+export default function DependencyTableClient({ dependencies,  dependents}) {
+    const [isDependency, setIsDependency] = useState(true);
+
     const dependency_headers = dependencies[1].length > 0 ? Object.keys(dependencies[1][0]).map(key => ({
         label: key.charAt(0).toUpperCase() + key.slice(1)
     })) : [];
@@ -25,17 +31,21 @@ export default async function DependencyTableClient({ dependencies,  dependents}
     return (
         <div>
             <ClickUIProvider theme={'dark'}>
-                <Tabs ariaLabel="dependencies and dependents" defaultValue="dependencies" className="h-full flex flex-col">
-                    <Tabs.TriggersList style={{"border": 0 }}>
-                        <Tabs.Trigger value="dependencies" key="dependencies">
-                            Dependencies
-                        </Tabs.Trigger>
-                        <Tabs.Trigger value="dependents" key="dependents" className="py-4">
-                            Dependents
-                        </Tabs.Trigger>
-                    </Tabs.TriggersList>
-
-                    <Tabs.Content value="dependencies" className="h-full">
+                <Tabs ariaLabel='dependencies and dependents' defaultValue='dependencies' className='h-full flex flex-col'>
+                    <div className='flex justify-between'>
+                        <Tabs.TriggersList style={{'border': 0 }} >
+                            <Tabs.Trigger value='dependencies' key='dependencies' onClick={()=>{setIsDependency(true);}}>
+                                Dependencies
+                            </Tabs.Trigger>
+                            <Tabs.Trigger value='dependents' key='dependents' className='py-4' onClick={()=>{setIsDependency(false);}}>
+                                Dependents
+                            </Tabs.Trigger>
+                        </Tabs.TriggersList>
+                        <Link href={isDependency ? dependencies[0]: dependents[0]} target='_blank' className='w-5 ml-5'>
+                            <ArrowTopRightOnSquareIcon className='h-5 w-5 flex-none icon-hover' aria-hidden='true'/>
+                        </Link>
+                    </div>
+                    <Tabs.Content value='dependencies' className='h-full'>
                         <ClickTable
                             headers={dependency_headers}
                             onSelect={() => {}}
@@ -43,7 +53,7 @@ export default async function DependencyTableClient({ dependencies,  dependents}
                             rows={dependency_rows}
                         />
                     </Tabs.Content>
-                    <Tabs.Content value="dependents" className="h-full">
+                    <Tabs.Content value='dependents' className='h-full'>
                         <ClickTable
                             headers={dependents_headers}
                             onSelect={() => {}}
