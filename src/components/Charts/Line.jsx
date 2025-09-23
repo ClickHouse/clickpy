037d01d10a86b725245d73382d3e38d03767380f
Lionel Palacin <lionel.palacin@clickhouse.com>
2025-09-23 16:14:29 +0200

    remove not working clear selection button fix tooltip over on line chart
    (cherry picked from commit 7a06c601c101e2254e1eda7d1b48b93f26a0806a)

--- src/components/Charts/Line.jsx
+++ src/components/Charts/Line.jsx
@@ -119,7 +119,7 @@ export default function Line({ data, onSelect, onClear, link }) {
       }
     },
     brush: {
-      toolbox: ['lineX', 'clear'],
+      toolbox: ['lineX'],
       brushType: 'lineX',
       brushMode: 'single',
       transformable: false
@@ -162,7 +162,7 @@ export default function Line({ data, onSelect, onClear, link }) {
   return (
     <div
       className='relative rounded-lg bg-slate-850 border border-slate-700 h-full justify-between flex flex-col'
-      onMouseMove={onMouseOver}
+      onMouseOver={onMouseOver}
       onMouseOut={onMouseOut}
       onDoubleClickCapture={onDoubleClick}>
       <div className='px-[4px] pt-[4px] flex-row flex justify-end'>
