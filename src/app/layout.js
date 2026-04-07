884dcaaf84ba92f8804d99dcd39b75b5c5b40db7
Lionel Palacin <lionel.palacin@clickhouse.com>
2026-04-07 14:52:14 +0100

    align cookie with clickhouse.com

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { ClickUIProvider } from '@/click-ui';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
-import GAWithConsent from '@/components/GAWithConsent';
+import HyperDXInit from '@/components/HyperDXInit';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,19 +14,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className='h-full antialiased'>
+      {/* Google Tag Manager */}
       <Script
-        defer
-        data-skip-css="false"
-        src="https://cdn-prod.securiti.ai/consent/cookie-consent-sdk-loader-strict-csp.js"
-        data-tenant-uuid="8555e54b-cd0b-45d7-9c1c-e9e088bf774a"
-        data-domain-uuid="03e5394d-77f1-4eff-8ca4-f893359476e5"
-        data-backend-url="https://app.securiti.ai"
+        id="gtm-script"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T55CC768');`
+        }}
       />
+      {/* End Google Tag Manager */}
 
       <body className={`${inter.className} h-full`}>
         <ClickUIProvider theme="dark">
-          <GAWithConsent>{children}</GAWithConsent>
-        </ClickUIProvider>        
+          <HyperDXInit>{children}</HyperDXInit>
+        </ClickUIProvider>
       </body>
     </html>
   );
