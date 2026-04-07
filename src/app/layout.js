ec0a730439a58d4bd8a4db0f7e12fe287eae84f8
Lionel Palacin <lionel.palacin@clickhouse.com>
2026-04-07 15:39:27 +0100

    fix console error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { ClickUIProvider } from '@/click-ui';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Script from 'next/script';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,20 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className='h-full antialiased'>
-      {/* Google Tag Manager */}
-      <Script
-        id="gtm-script"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T55CC768');`
-        }}
-      />
-      {/* End Google Tag Manager */}
-
+      <GoogleTagManager gtmId="GTM-T55CC768" />
       <body className={`${inter.className} h-full`}>
         <ClickUIProvider theme="dark">
           {children}
